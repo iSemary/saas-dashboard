9ac32f1f6462a003ffb12497149df60dfc0d1157
Abdelrahman Mostafa <abdelrahmandeveloper123@gmail.com>
2025-01-20 20:09:15 +0200

    Clean up vite

--- vite.config.js
+++ vite.config.js
@@ -2,54 +2,16 @@
 
 import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
-import path from "path";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: [
-                "resources/js/app.jsx",
-                // Landlord assets
-                "resources/css/landlord/app.css",
-                "resources/css/landlord/rtl.css",
-                // Tenant assets
-                "resources/css/tenant/app.css",
-                "resources/css/tenant/rtl.css",
-            ],
+            input: ["resources/js/app.jsx"],
             refresh: true,
         }),
     ],
-    build: {
-        rollupOptions: {
-            output: {
-                manualChunks: {
-                    landlord: ["resources/css/landlord/app.css"],
-                    tenant: ["resources/css/tenant/app.css"],
-                },
-            },
-        },
-    },
-    css: {
-        preprocessorOptions: {
-            sass: {
-                api: "modern",
-                charset: false,
-                quietDeps: true,
-                includePaths: [
-                    path.resolve(__dirname, "node_modules"),
-                ],
-            },
-            scss: {
-                api: "modern",
-                charset: false,
-                quietDeps: true,
-                includePaths: [
-                    path.resolve(__dirname, "node_modules"),
-                ],
-            },
-        },
-    },
-    resolve: {
-        dedupe: ['slick-carousel'],
+    server: {
+        origin: 'http://localhost:5173',
+        cors: true
     },
 });
